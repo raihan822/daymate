932b17c6f2a5ba5d033d8c1f8754da389797a2e2
raihan822 <raihansarker820@gmail.com>
2026-03-02 20:01:39 +0600

    Updated02Mar

--- backend/app/src/__init__.py
+++ backend/app/src/__init__.py
@@ -1,5 +1,11 @@
-from . import ai_integration, services
+from . import ai_integration, services  #sub-folders
 __all__ = ['ai_integration', 'services']
 
-# Now, main.py te chaile just src import * korlei hoy shob chole ashbe
-    # from backend.app.src import *
+"""
+# The `from backend.app.src import *` to import will not work.
+
+But still, it is recomended to keep this __init__
+just to let python understand that the whole src/ dir is a pkg and treat like that.
+
+you can keep the this init empty. or keep what is written
+"""
