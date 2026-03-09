51ca3d7bd72e124af360e23ed55d15436e26e6da
raihan822 <raihansarker820@gmail.com>
2026-03-09 12:01:31 +0600

    daymate 9mar,26: updated coors middlewire in backend

--- daymate/src/pages/Weather.jsx
+++ daymate/src/pages/Weather.jsx
@@ -1,4 +1,14 @@
+import {BASE_URL} from "../api/baseUrl.js";
+import useFetch from "../hook/useFetch.jsx";
+import useGeoLocation from "../hook/useGeoLocation.jsx";
+
+
 export default function Weather(){
+    const {location, locError, isLocLoading, fetchLocation} = useGeoLocation();
+
+    // WEATHER:
+    const {data:weather, loading: isWeatherLoading, fetchData: fetchWeather} = useFetch(`${BASE_URL}/weather`, {params:{lat: location.lat, lon: location.lon}}, false);
+
     return (
         <div>
             <h1>This is the Weather page</h1>
